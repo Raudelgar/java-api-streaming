97b42d7abf4777d9cee57e3a969d5e4009d4b9e8
Raudel Garcia <raudel24@gmail.com>
2018-10-10 00:10:43 -0400

    Reading FX instruments from OANDA API Stream

--- src/main/java/JavaApiStreaming.java
+++ src/main/java/JavaApiStreaming.java
@@ -11,25 +11,29 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.client.HttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 public class JavaApiStreaming {
-    public static void main (String[]args) throws IOException {
+    @SuppressWarnings("deprecation")
+	public static void main (String[]args) throws IOException {
 
         HttpClient httpClient = HttpClientBuilder.create().build();
 
         try {
 
             // Set these variables to whatever personal ones are preferred
-            String domain = "https://stream-fxpractice.oanda.com";
-            String access_token = "ACCESS-TOKEN";
-            String account_id = "1234567";
+            String domain = "https://api-fxpractice.oanda.com";
+            String domainv20 = "/v3/accounts/";
+            String endPointPrice = "/pricing?instruments=";
+            String access_token = "aafefcd10c35cc034f8890f2aa8c02d0-cc731cea4a7467697e343cb9fb36724b";
+            String account_id = "101-001-9464198-001";
             String instruments = "EUR_USD,USD_JPY,EUR_JPY";
+            String eur_usd = "EUR_USD";
+//https://api-fxpractice.oanda.com/v3/accounts/101-001-9464198-001/pricing?instruments=USD_CAD
 
-
-            HttpUriRequest httpGet = new HttpGet(domain + "/v1/prices?accountId=" + account_id + "&instruments=" + instruments);
+            HttpUriRequest httpGet = new HttpGet(domain+domainv20+account_id+endPointPrice+instruments);
             httpGet.setHeader(new BasicHeader("Authorization", "Bearer " + access_token));
 
             System.out.println("Executing request: " + httpGet.getRequestLine());
@@ -45,26 +49,37 @@ public class JavaApiStreaming {
                 while ((line = br.readLine()) != null) {
 
                     Object obj = JSONValue.parse(line);
-                    JSONObject tick = (JSONObject) obj;
+                    JSONObject quotes = (JSONObject) obj;
 
                     // unwrap if necessary
-                    if (tick.containsKey("tick")) {
-                        tick = (JSONObject) tick.get("tick");
-                    }
-
-                    // ignore heartbeats
-                    if (tick.containsKey("instrument")) {
-                        System.out.println("-------");
-
-                        String instrument = tick.get("instrument").toString();
-                        String time = tick.get("time").toString();
-                        double bid = Double.parseDouble(tick.get("bid").toString());
-                        double ask = Double.parseDouble(tick.get("ask").toString());
-
-                        System.out.println(instrument);
-                        System.out.println(time);
-                        System.out.println(bid);
-                        System.out.println(ask);
+                    if (quotes.containsKey("prices")) {
+                    	JSONArray pricesArray = (JSONArray)quotes.get("prices");
+                    	int count = pricesArray.size();
+                    	int i = 0;
+                    	while(count > 0) {
+                    		JSONObject instrumentObj = (JSONObject)pricesArray.get(i);
+                    		JSONArray askArray = (JSONArray)instrumentObj.get("asks");
+                    		JSONObject askObj = (JSONObject)askArray.get(0);
+                    		JSONArray bidArray = (JSONArray)instrumentObj.get("bids");
+                    		JSONObject bidObj = (JSONObject)bidArray.get(0);
+                        	String instrument = instrumentObj.get("instrument").toString();
+                        	String time = instrumentObj.get("time").toString();
+                        	double bid = Double.parseDouble(bidObj.get("price").toString());
+                        	double ask = Double.parseDouble(askObj.get("price").toString());
+                        	double closeoutBid = Double.parseDouble(instrumentObj.get("closeoutBid").toString());
+                        	double closeoutAsk = Double.parseDouble(instrumentObj.get("closeoutAsk").toString());
+                        	
+                        	System.out.println("-------");
+                        	System.out.println("Instrument: "+instrument);
+                            System.out.println("Time: "+time);
+                            System.out.println("Bid: "+bid);
+                            System.out.println("Ask: "+ask);
+                            System.out.println("CloseOutBid: "+closeoutBid);
+                            System.out.println("CloseOutAsk: "+closeoutAsk);
+                     
+                            i++;
+                            count--;
+                    	}
                     }
                 }
             } else {
